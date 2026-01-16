a4418e111188b030e1aa3ed0b232a77ccad06660
James Cook <jamesjcook@users.noreply.github.com>
2026-01-16 17:17:51 -0500

    update to localizer method

--- src/bin/localizer.rs
+++ src/bin/localizer.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 use mr_units::constants::Nucleus::Nuc1H;
 use mr_units::primitive::{Angle, FieldGrad, Freq, Length, Time};
 use mr_units::quantity::Unit;
-use mrs_ppl::compile::compile_seq;
+use mrs_ppl::compile::{build_seq, compile_seq};
 use seq_struct::acq_event::ACQEvent;
 use seq_struct::grad_strength::EventControl;
 use seq_struct::gradient_event::GradEvent;
@@ -16,20 +16,17 @@ use seq_lib::grad_pulses::{ramp_down, ramp_up, trapezoid};
 use seq_lib::{rf_pulses, PulseSequence};
 use seq_lib::defs::{VIEW, SLICE, RF, GW, GS, RFP};
 
-enum Plane {
-    XY,
-    XZ,
-    YZ,
-}
-
-enum Mode {
-    ThreePlane,
-    //OnePlane{plane:Plane},
+fn main() {
+    let out_dir = r"D:\dev\test\260116";
+    let mut localizer = Localizer::default();
+    localizer.setup_mode = false;
+    let localizer = localizer.compile();
+    compile_seq(&localizer, out_dir, "localizer", false);
+    build_seq(out_dir)
 }
 
 struct Localizer {
     /// determines the mode to compile the sequence in
-    mode: Mode,
     bandwidth_khz: f64,
     n_samples: usize,
     fov: f64,
@@ -37,19 +34,20 @@ struct Localizer {
     rf_duration_us: usize,
     grad_ramp_time_us: usize,
     phase_enc_dur_ms: f64,
+    setup_mode: bool,
 }
 
 impl Default for Localizer {
     fn default() -> Self {
         Self {
-            mode: Mode::ThreePlane,
             bandwidth_khz: 100.0,
             n_samples: 256,
             fov: 25.6,
-            slice_thickness_mm: 1.,
-            rf_duration_us: 1000,
-            grad_ramp_time_us: 100,
+            slice_thickness_mm: 0.1,
+            rf_duration_us: 1500,
+            grad_ramp_time_us: 500,
             phase_enc_dur_ms: 0.5,
+            setup_mode: false
         }
     }
 }
@@ -59,6 +57,7 @@ struct Waveforms {
     ru: GW,
     rd: GW,
     pe: GW,
+    sl: GW,
 }
 
 impl Waveforms {
@@ -89,11 +88,18 @@ impl Waveforms {
             grad_dt
         ).to_shared();
 
+        let sl = trapezoid(
+            Time::us(params.grad_ramp_time_us),
+            Time::us(params.rf_duration_us),
+            grad_dt
+        ).to_shared();
+
         Waveforms {
             rf_pulse,
             ru,
             rd,
             pe,
+            sl,
         }
 
     }
@@ -106,6 +112,7 @@ struct EventControllers {
     gpe: GS,
     gpre: GS,
     rf: RFP,
+    gsc: GS,
 }
 
 impl EventControllers {
@@ -132,24 +139,32 @@ impl EventControllers {
 
         let gsl = EventControl::<FieldGrad>::new().with_constant_grad(g_sl).to_shared();
 
-        let m1:Time = (waveforms.rf_pulse.duration() + rt).try_into().unwrap();
+        let m1:Time = (Time::us(params.rf_duration_us) + rt).try_into().unwrap();
         let m2:Time = (Time::ms(params.phase_enc_dur_ms) + rt).try_into().unwrap();
         let f = m1 / m2;
-        let g_slr = gread.scale(-f.si() / 2.);
-        let gslr = EventControl::<FieldGrad>::new().with_constant_grad(g_slr).to_shared();
+        let g_slr = g_sl.scale(-f.si() / 2.);
+        let gslr = EventControl::<FieldGrad>::new().with_constant_grad(g_slr).with_adj("ssr").to_shared();
 
         let tpe:Time = (Time::ms(params.phase_enc_dur_ms) + rt).try_into().unwrap();
         let gpe_step = FieldGrad::from_fov(Length::mm(params.fov),tpe,Nuc1H);
 
-        let mut steps = vec![];
-        for p in 0..params.n_samples as i32 {
-            steps.push(p - p/2)
-        }
+        let steps = if params.setup_mode {
+            vec![0;params.n_samples]
+        }else {
+            let mut steps = vec![];
+            for p in 0..params.n_samples as i32 {
+                steps.push(p - params.n_samples as i32/2)
+            }
+            steps
+        };
+
 
         let pe_lut = LUT::new("pelut",&steps).to_shared();
-        let gpe = EventControl::<FieldGrad>::new().with_lut(&pe_lut).with_grad_scale(gpe_step).to_shared();
+        let gpe = EventControl::<FieldGrad>::new().with_source_loop("view").with_lut(&pe_lut).with_grad_scale(gpe_step).to_shared();
 
         let rf = EventControl::<f64>::new().with_adj("rf_pow").to_shared();
+
+        let gsc = EventControl::<FieldGrad>::new().with_adj("gsc").to_shared();
 
         EventControllers {
             gro,
@@ -158,41 +173,46 @@ impl EventControllers {
             gpe,
             gpre,
             rf,
+            gsc,
         }
 
     }
 }
 
 struct Events {
-    ssru: GradEvent,
-    ssrd: GradEvent,
+    sl: GradEvent,
     alpha: RfEvent,
     pe_ssr: GradEvent,
     roru: GradEvent,
     acq: ACQEvent,
     rord: GradEvent,
+    gsc: GradEvent,
 }
 
 impl Events {
     fn build(params:&Localizer, w: &Waveforms, ec: &EventControllers) -> Events {
 
-        let ssru = GradEvent::new("ssru").with_z(&w.ru).with_strength_z(&ec.gsl);
-        let ssrd = GradEvent::new("ssrd").with_z(&w.rd).with_strength_z(&ec.gsl);
-        let pe_ssr = GradEvent::new("pe_ssr").with_y(&w.pe).with_strength_y(&ec.gpe).with_z(&w.pe).with_strength_z(&ec.gslr);
+        let sl = GradEvent::new("sl").with_z(&w.sl).with_strength_z(&ec.gsl);
+
+        let pe_ssr = GradEvent::new("pe_ssr").with_x(&w.pe).with_strength_x(&ec.gpre).with_y(&w.pe).with_strength_y(&ec.gpe).with_z(&w.pe).with_strength_z(&ec.gslr);
         let alpha = RfEvent::new("alpha",&w.rf_pulse,&ec.rf);
 
         let roru = GradEvent::new("roru").with_x(&w.ru).with_strength_x(&ec.gro);
         let rord = GradEvent::new("rord").with_x(&w.rd).with_strength_x(&ec.gro);
         let acq = ACQEvent::new("acq",params.n_samples,Freq::khz(params.bandwidth_khz).inv());
 
+        let gsc = GradEvent::new("gsc").with_x(&w.pe).with_strength_x(&ec.gsc).with_y(&w.pe).with_strength_y(&ec.gsc).with_z(&w.pe).with_strength_z(&ec.gsc);
+
+
+
         Events {
-            ssru,
-            ssrd,
+            sl,
             alpha,
             pe_ssr,
             roru,
             acq,
             rord,
+            gsc,
         }
     }
 }
@@ -206,36 +226,43 @@ impl PulseSequence for Localizer {
 
         let mut sl = SeqLoop::new_main("slice",3);
 
-        // 3-plane localizer rotations
-        let orient = [
-            [Angle::deg(0),Angle::deg(0),Angle::deg(0)],
-            [Angle::deg(90),Angle::deg(0),Angle::deg(0)],
-            [Angle::deg(0),Angle::deg(90),Angle::deg(0)],
+        if self.setup_mode {
+            let o = [
+                [Angle::deg(0),Angle::deg(0),Angle::deg(0)],
             ];
+            sl.set_orientations(Orientations::new(&o));
+        }else {
+            let o = [
+                [Angle::deg(0),Angle::deg(0),Angle::deg(0)],
+                [Angle::deg(0),Angle::deg(90),Angle::deg(0)],
+                [Angle::deg(0),Angle::deg(0),Angle::deg(90)],
+            ];
+            sl.set_orientations(Orientations::new(&o));
+        };
 
-        sl.set_orientations(Orientations::new(&orient));
+        // 3-plane localizer rotations
 
-        sl.add_event(events.ssru).unwrap();
+        sl.add_event(events.sl).unwrap();
         sl.add_event(events.alpha).unwrap();
-        sl.add_event(events.ssrd).unwrap();
         sl.add_event(events.pe_ssr).unwrap();
         sl.add_event(events.roru).unwrap();
         sl.add_event(events.acq).unwrap();
         sl.add_event(events.rord).unwrap();
+        sl.add_event(events.gsc).unwrap();
 
 
-        sl.set_time_span("ssru","alpha",100,0,Time::us(100)).unwrap();
-        sl.set_min_time_span("alpha","ssrd",100,0,Time::us(0)).unwrap();
-        sl.set_time_span("ssrd","pe_ssr",100,0,Time::us(200)).unwrap();
+        sl.set_time_span("sl","alpha",50,50,Time::us(0)).unwrap();
+        sl.set_time_span("sl","pe_ssr",100,0,Time::us(200)).unwrap();
         sl.set_time_span("pe_ssr","roru",100,0,Time::us(200)).unwrap();
         sl.set_min_time_span("roru","acq",100,0,Time::us(0)).unwrap();
         sl.set_min_time_span("acq","rord",100,0,Time::us(0)).unwrap();
-        sl.set_rep_time(Time::ms(20)).unwrap();
-        sl.set_pre_calc(Time::ms(2));
+        sl.set_min_time_span("rord","gsc",100,0,Time::us(0)).unwrap();
+        sl.set_rep_time(Time::ms(10)).unwrap();
+        sl.set_pre_calc(Time::ms(4));
         let mut vl = SeqLoop::new("view", self.n_samples);
         vl.set_pre_calc(Time::ms(2));
         vl.add_loop(sl).unwrap();
-        vl.set_rep_time(Time::ms(70)).unwrap();
+        vl.set_rep_time(Time::ms(50)).unwrap();
         vl
 
     }
@@ -243,6 +270,7 @@ impl PulseSequence for Localizer {
     fn adjustment_state(&self) -> HashMap<String, f64> {
         let mut adj = HashMap::new();
         adj.insert("rf_pow".to_string(),1.);
+        adj.insert("ssr".to_string(),0.);
         adj
     }
 }
@@ -250,8 +278,3 @@ impl PulseSequence for Localizer {
 
 
 
-fn main() {
-
-    let seqloop = Localizer::default().compile();
-
-}
